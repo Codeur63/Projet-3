99d193c168ed962e3514eabcb32d324e9a65662a
Codeur63 <Codeur63@gmail.com>
2026-07-27 17:59:48 +0100

    feat: Add utils

---
+++ utils/utils.py
@@ -0,0 +1,19 @@
+import joblib
+import json
+
+def save_model(model,path):
+    joblib.dump(
+        model,
+        path
+    )
+
+def load_model(path):
+    return joblib.load(path)
+
+def save_metrics(metrics,path):
+    with open(path,"w") as f:
+        json.dump(
+            metrics,
+            f,
+            indent=4
+        )
